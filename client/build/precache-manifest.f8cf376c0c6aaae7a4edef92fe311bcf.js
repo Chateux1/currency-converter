719e5f5a70fbc6997f6552dbe63f1cbde7c62768
Chateux1 <m.satas@gmail.com>
2020-10-04 23:20:21 +0300

    Add client `build`

---
+++ client/build/precache-manifest.f8cf376c0c6aaae7a4edef92fe311bcf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "350bacc660886bdf39b2c0cb83bbec59",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4387d3b7b548ba3e298e",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "c283d2899a94738952cb",
+    "url": "/static/css/main.4557026f.chunk.css"
+  },
+  {
+    "revision": "4387d3b7b548ba3e298e",
+    "url": "/static/js/2.6e28f718.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.6e28f718.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c283d2899a94738952cb",
+    "url": "/static/js/main.86c6449f.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
